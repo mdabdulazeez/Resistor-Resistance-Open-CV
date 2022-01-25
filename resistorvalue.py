d8cca20945c523af8df58ce2b0927927d1c9bcad
Veeralakshmi <87717206+Veera-lakshmi@users.noreply.github.com>
2022-01-25 11:50:36 +0530

    Add files via upload

--- resistorvalue.py
+++ resistorvalue.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import cvzone
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+
 
 '''cap = cv2.VideoCapture(0)
 frameWidth = 640
@@ -8,26 +11,42 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
 '''
-path='../sampleeeee/res4.jpg'
+segmentor = SelfiSegmentation()
+path='../sampleeeee/res1.jpg'
+rescascade = cv2.CascadeClassifier("../sampleeeee/haarCascade/cascade.xml")
 
-colors = [[0, 90, 40,15, 255, 255],
-          [10, 70, 70,25, 255, 200],
-          [23,117,170,94,255,255],
-          [65,41,75,114,255,255],
-          [120,54,77,179,255,219]
-          ]
+colors = [[(65,41,75),(114,255,255),6],
+          [(0,90,40),(15,255,255),1],
+          [(0,0,50),(179,50,198),8]]
 
-def findColors(img,colors):
-    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+def findColors(imgout,colors):
+    imgHSV = cv2.cvtColor(imgout,cv2.COLOR_BGR2HSV)
     for color in colors:
-        lower = np.array(color[0:3])
-        upper = np.array(color[3:6])
+        lower = np.array(color[0])
+        upper = np.array(color[1])
         mask = cv2.inRange(imgHSV, lower, upper)
         #cv2.imshow(str(color[0]),mask)
         getContours(mask)
 
-def getContours(img):
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+        '''pre_bil = cv2.bilateralFilter(img, 5, 80, 80)
+
+        thresh = cv2.adaptiveThreshold(cv2.cvtColor(pre_bil, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                       cv2.THRESH_BINARY, 59, 5)
+        thresh = cv2.bitwise_not(thresh)
+
+        #cv2.imshow("pre_bil", thresh)
+        result = cv2.bitwise_or(mask,thresh,mask=mask)'''
+
+
+
+        #cv2.imshow("Result",result)
+
+
+
+def getContours(imgout):
+
+    contours,hierarchy = cv2.findContours(imgout,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
@@ -38,12 +57,32 @@ def getContours(img):
             x,y,w,h = cv2.boundingRect(approx)
 
 
+
 while True:
     #success, img = cap.read()
     img = cv2.imread(path)
-    imgResult = img.copy()
-    findColors(img, colors)
+
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resistor = rescascade.detectMultiScale(imgGray,1.03,4)
+    for (x, y, w, h) in resistor:
+        area = w*h
+        if area > 200:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(img,"resistor",(x,y-5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0),2)
+            imgRoi = img[y:y+h,x:x+w]
+
+            cv2.imshow("ROI", imgRoi)
+    imgout = segmentor.removeBG(imgRoi, (0, 0, 0), threshold=0.4)
+    imgResult = imgout.copy()
+
+    findColors(imgout, colors)
     cv2.imshow('Result',imgResult)
+    cv2.imshow("result1",img)
+    cv2.imshow("result2",imgout)
+
+
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 #cap.release()
